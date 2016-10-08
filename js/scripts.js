ed1e89bcf88b2b4c8e147a9287747f90053e7cad
Pablo Vera <pablovera.pdx@gmail.com>
2016-10-07 18:48:27 -0700

    make nav hide when scrolling down and show when scrolling up

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,39 @@
+// Collapsable navbar 
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
 });
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    lastScrollTop = st;
+}
