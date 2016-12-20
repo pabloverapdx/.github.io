b735eab671e577e9af0f2bb079acbb507c01df60
Pablo Vera <pablovera.pdx@gmail.com>
2016-12-19 17:54:11 -0800

    front page is working the way I want to - remember to comeback to this commit if things break down later on

--- js/scripts.js
+++ js/scripts.js
@@ -33,14 +33,14 @@ $(document).ready(function() {
   $('.thumbnail-item').click(function() {
     $('.project-thumbnail').hide();
 
-    $thumbnail_item = jQuery(this);
+    $thumbnail_item = $(this);
 
     var thumbnail_item_id = parseInt($thumbnail_item.attr('id').replace('thumbnail', ''), 10);
 
     var $projects = $('.projects');
     $projects.each(function() {
 
-      $div = jQuery(this);
+      $div = $(this);
       var div_id = parseInt($div.attr('id').replace('project', ''), 10);
 
       $div.fadeOut(500, function() {
@@ -55,17 +55,17 @@ $(document).ready(function() {
 
 $('.list').click(function(){
   $('.project-thumbnail').hide()
-    $list = jQuery(this);
+    $list = $(this);
     var list_id = parseInt($list.attr('id').replace('list', ''), 10);
     var $projects = $('.projects');
 
     $projects.each(function(){
-      $div = jQuery(this);
+      $div = $(this);
       var div_id = parseInt($div.attr('id').replace('project', ''), 10);
 
       $div.hide().promise().done(function(){
         if (div_id === list_id) {
-          var $selectedDiv = jQuery(this);
+          var $selectedDiv = $(this);
           $selectedDiv.fadeIn(500);
         }
     });
